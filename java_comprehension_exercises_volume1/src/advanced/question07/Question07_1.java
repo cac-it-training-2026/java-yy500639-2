7caf3e9b4ab5c14180966d5a11b9aee598c5dcd9
y-yasuda <y-yasuda@CD26311L.arksystems.co.jp>
2026-04-16 12:07:12 +0900

    7-1

--- java_comprehension_exercises_volume1/src/advanced/question07/Question07_1.java
+++ java_comprehension_exercises_volume1/src/advanced/question07/Question07_1.java
@@ -1,12 +1,37 @@
 package advanced.question07;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question07_1 {
 
 	public static void main(String[] args) throws IOException {
 
 		// TODO:ここに実装
+		String[] itemNames = new String[5];
+		int[] prices = new int[5];
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str;
+
+		for (int i = 0; i < 5; i++) {
+			System.out.print("商品" + (i + 1) + "を入力してください:>");
+			itemNames[i] = br.readLine();
+		}
+
+		System.out.println("各商品の単価を順に入力してください");
+		for (int i = 0; i < 5; i++) {
+			System.out.print(itemNames[i] + "の単価:>");
+			str = br.readLine();
+			prices[i] = Integer.parseInt(str);
+		}
+
+		System.out.println("\n登録商品一覧");
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println(itemNames[i] + "：" + prices[i] + "円");
+		}
 
 	}
 
