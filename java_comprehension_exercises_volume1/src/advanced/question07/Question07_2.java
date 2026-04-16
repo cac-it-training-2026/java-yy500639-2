75608ed7807437e7680bd697d495eac1ba1bb25b
y-yasuda <y-yasuda@CD26311L.arksystems.co.jp>
2026-04-16 12:16:35 +0900

    7-2

--- java_comprehension_exercises_volume1/src/advanced/question07/Question07_2.java
+++ java_comprehension_exercises_volume1/src/advanced/question07/Question07_2.java
@@ -1,6 +1,8 @@
 package advanced.question07;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question07_2 {
 
@@ -9,9 +11,21 @@ public class Question07_2 {
 		// 登録済み商品（値は変更可）
 		String[] itemNames = { "ノート", "ボールペン", "消しゴム", "はさみ", "のり" };
 		// 単価（値は変更可）
-		int[] prices = { 100, 200, 300, 400, 500 };
+		int[] prices = { 120, 80, 50, 250, 100 };
 
 		// TODO: 実装ここから
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] amounts = new int[5];
+		String str;
+
+		for (int i = 0; i < itemNames.length; i++) {
+			System.out.print(itemNames[i] + "の購入数:>");
+			str = br.readLine();
+			amounts[i] = Integer.parseInt(str);
+		}
+
+		for (int i = 0; i < itemNames.length; i++) {
+			System.out.println(itemNames[i] + "：" + (prices[i] * amounts[i]) + "円");
+		}
 	}
 }
