03b729a89006876610188a36cf0a1a5583a880ef
y-yasuda <y-yasuda@CD26311L.arksystems.co.jp>
2026-04-16 12:09:02 +0900

    7-1修正

--- java_comprehension_exercises_volume1/src/advanced/question07/Question07_1.java
+++ java_comprehension_exercises_volume1/src/advanced/question07/Question07_1.java
@@ -15,13 +15,13 @@ public class Question07_1 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str;
 
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < itemNames.length; i++) {
 			System.out.print("商品" + (i + 1) + "を入力してください:>");
 			itemNames[i] = br.readLine();
 		}
 
 		System.out.println("各商品の単価を順に入力してください");
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < itemNames.length; i++) {
 			System.out.print(itemNames[i] + "の単価:>");
 			str = br.readLine();
 			prices[i] = Integer.parseInt(str);
