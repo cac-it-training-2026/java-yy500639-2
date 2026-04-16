8a19a0474600b96a991d36df4b9993aaaf818c5d
y-yasuda <y-yasuda@CD26311L.arksystems.co.jp>
2026-04-16 14:06:39 +0900

    7-5

--- java_comprehension_exercises_volume1/src/advanced/question07/Question07_5.java
+++ java_comprehension_exercises_volume1/src/advanced/question07/Question07_5.java
@@ -1,6 +1,8 @@
 package advanced.question07;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question07_5 {
 
@@ -14,5 +16,28 @@ public class Question07_5 {
 		int[] amounts = new int[5];
 
 		// TODO: 実装ここから
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str;
+		int totalPrices = 0;
+		int[] priceWithTax = new int[itemNames.length];
+
+		for (int i = 0; i < itemNames.length; i++) {
+			System.out.print(itemNames[i] + "の購入数:>");
+			str = br.readLine();
+			amounts[i] = Integer.parseInt(str);
+			priceWithTax[i] = (int) (prices[i] * 1.1);
+			totalPrices = totalPrices + priceWithTax[i] * amounts[i];
+		}
+
+		System.out.println("購入内容一覧：");
+		for (int i = 0; i < itemNames.length; i++) {
+			System.out.println(itemNames[i] + "：単価" + prices[i] + "円（税込" + priceWithTax[i] + "円）×" + amounts[i] + "個 = "
+					+ (priceWithTax[i] * amounts[i]) + "円");
+		}
+		System.out.println("合計（税込）：" + totalPrices + "円");
+
+		if (totalPrices >= 5000) {
+			System.out.println("割引後合計（税込）：" + (int) (totalPrices * 0.9) + "円");
+		}
 	}
 }
