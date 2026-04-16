29a4f753e9d79ef65075f98d3db930bd4b50d2be
y-yasuda <y-yasuda@CD26311L.arksystems.co.jp>
2026-04-16 10:42:55 +0900

    2-1

--- java_comprehension_exercises_volume1/src/basic/question02/Question02_1.java
+++ java_comprehension_exercises_volume1/src/basic/question02/Question02_1.java
@@ -1,6 +1,8 @@
 package basic.question02;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 第5章 入出力
@@ -11,6 +13,16 @@ public class Question02_1 {
 	public static void main(String[] args) throws IOException {
 
 		// TODO:ここに実装
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("名前を入力してください。");
+		String name = br.readLine();
+
+		System.out.println("好きなスポーツを入力してください。");
+		String sport = br.readLine();
+
+		System.out.println("\n私の名前は" + name + "です。");
+		System.out.println("好きなスポーツは" + sport + "です");
 
 	}
 }
