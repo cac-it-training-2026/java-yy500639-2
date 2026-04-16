7d78cfb9d71290fcf63ea21d489646b52d0dd819
y-yasuda <y-yasuda@CD26311L.arksystems.co.jp>
2026-04-16 11:31:45 +0900

    5-4

--- java_comprehension_exercises_volume1/src/basic/question05/Question05_4.java
+++ java_comprehension_exercises_volume1/src/basic/question05/Question05_4.java
@@ -1,6 +1,8 @@
 package basic.question05;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 第8章 条件分岐
@@ -11,6 +13,24 @@ public class Question05_4 {
 	public static void main(String[] args) throws IOException {
 
 		// TODO:ここに実装
+		System.out.println("数学の試験結果の点数を入力してください。");
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		int math = Integer.parseInt(str);
+
+		System.out.println("国語の試験結果の点数を入力してください。");
+
+		str = br.readLine();
+		int japanese = Integer.parseInt(str);
+
+		if (math >= 60 && japanese >= 60) {
+			System.out.println("数学と国語ともに合格です。");
+		} else if (math >= 60 || japanese >= 60) {
+			System.out.println("数学、国語どちらかが合格です。");
+		} else {
+			System.out.println("どちらの教科不合格です。");
+		}
 
 	}
 }
